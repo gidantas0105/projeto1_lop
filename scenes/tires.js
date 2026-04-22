81dae2f139292dd0ff42c92da6cfd455af595b21
Giovana <giovanamoratelli@gmail.com>
2026-04-22 12:11:51 -0300

    feat: adds car

--- scenes/tires.js
+++ scenes/tires.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 // Setting rendener, scene and camera
 const scene = new THREE.Scene();
@@ -9,37 +10,61 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-let mouseX = 0;
-let mouseY = 0;
+const controls = new OrbitControls(camera, renderer.domElement);
 
-document.addEventListener('mousemove', function(e) {
-    mouseX = (e.clientX / window.innerWidth) /100;
-    mouseY = (e.clientY / window.innerHeight) / 100;
-})
+camera.position.y = 50;
+camera.position.x = 50;
+camera.position.z = 75;
 
-const tireGeometry = new THREE.TorusGeometry( 5, 2.5, 16, 100 );
-const tireMaterial = new THREE.MeshBasicMaterial( { color: 0x111111 } );
-
-const tires = [];
-
-for (let i = 0; i < 4; i++) {
-    const tire = new THREE.Mesh( tireGeometry, tireMaterial );
-    tire.rotation.y = Math.PI / 2 ;
-    tires.push(tire);
-    scene.add(tire);
-}
-
-tires[0].position.set( -10, 15, 0 );
-tires[1].position.set( 10, 15, 0 );
-tires[2].position.set( -10, -15, 0 );
-tires[3].position.set( 10, -15, 0 );
-
-camera.position.y = -100;
-camera.lookAt( 0, 0, 0 );
+const car = createCar();
+scene.add(car);
 
 function animate( time ) {
-    // camera.position.x += (mouseX - camera.position.x);
-    // camera.position.y += (-mouseY - camera.position.y);
+    controls.update();
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
+
+function createWheels() {
+    const tireGeometry = new THREE.TorusGeometry( 5, 2.5, 16, 100 );
+    const tireMaterial = new THREE.MeshBasicMaterial( { color: 0x111111 } );
+
+    const tires = [];
+
+    for (let i = 0; i < 4; i++) {
+        const tire = new THREE.Mesh( tireGeometry, tireMaterial );
+        tire.rotation.y = Math.PI;
+        tires.push(tire);
+    }
+
+    tires[0].position.set( -18, 5, 15 );
+    tires[1].position.set( 18, 5, 15 );
+    tires[2].position.set( -18, 5, -15 );
+    tires[3].position.set( 18, 5, -15 );
+
+    return tires;
+}
+
+function createCar() {
+    const car = new THREE.Group();
+
+    const wheels = createWheels();
+    car.add(...wheels);
+
+    const main = new THREE.Mesh(
+        new THREE.BoxGeometry(60, 15, 30),
+        new THREE.MeshBasicMaterial({ color: 0xff9bb3 })
+    );
+    main.position.y = 12;
+    car.add(main);
+
+    const cabin = new THREE.Mesh(
+        new THREE.BoxGeometry(33, 12, 24),
+        new THREE.MeshBasicMaterial({ color: 0xfff4e6 })
+    );
+    cabin.position.x = -6;
+    cabin.position.y = 25.5;
+    car.add(cabin);
+
+    return car;
+}
