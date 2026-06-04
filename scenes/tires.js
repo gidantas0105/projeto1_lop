a26a9a97fc465ace83e6c7c7319a2971f7fe3477
Giovana <giovanamoratelli@gmail.com>
2026-06-04 15:41:12 -0300

    Revert "feat: adds posenet loading and tracking"
    This reverts commit e975231b610ee903f37ab4017d10c7e4570138da.

--- scenes/tires.js
+++ scenes/tires.js
@@ -6,7 +6,6 @@ import * as handpose from 'https://cdn.jsdelivr.net/npm/@tensorflow-models/handp
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xadd8e6 );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(75, 50, 50);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
